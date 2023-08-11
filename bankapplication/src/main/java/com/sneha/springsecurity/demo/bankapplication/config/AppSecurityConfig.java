a24ed9e7185b6cfed1f9f1175b8cfa3933446be7
SNEHA N <sn263@dxc.com>
2023-08-11 10:11:40 +0530

    Added CORS config

--- bankapplication/src/main/java/com/sneha/springsecurity/demo/bankapplication/config/AppSecurityConfig.java
+++ bankapplication/src/main/java/com/sneha/springsecurity/demo/bankapplication/config/AppSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.sneha.springsecurity.demo.bankapplication.config;
 
+import java.util.Collections;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,8 +15,13 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
 import com.sneha.springsecurity.demo.bankapplication.config.custom.BankAppAuthorityLoggingFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AppSecurityConfig {
@@ -21,7 +29,21 @@ public class AppSecurityConfig {
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.csrf().disable().addFilterAfter(new BankAppAuthorityLoggingFilter(), BasicAuthenticationFilter.class)
+		http.securityContext().requireExplicitSave(false).and().sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+		.cors().configurationSource(new CorsConfigurationSource() {
+			
+			@Override
+			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+				CorsConfiguration config = new CorsConfiguration();
+				config.setAllowedOrigins(Collections.singletonList("*"));
+				config.setAllowedMethods(Collections.singletonList("*"));
+				config.setAllowCredentials(true);
+				config.setAllowedHeaders(Collections.singletonList("*"));
+				config.setMaxAge(3600L);
+				return config;
+			}
+		})
+		.and().csrf().disable().addFilterAfter(new BankAppAuthorityLoggingFilter(), BasicAuthenticationFilter.class)
 		.authorizeHttpRequests()
 //		.requestMatchers("/accounts").hasAuthority("VIEWACCOUNT")
 //		.requestMatchers("/balance").hasAuthority("VIEWBALANCE")
